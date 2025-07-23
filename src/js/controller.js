963ecba8592338e87786136621c8e6895955011e
GeorgeBanias <georgebanias28@gmail.com>
2025-07-23 14:03:01 +0300

    Clenead up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -129,9 +129,7 @@ const controlAddRecipe = async function (newRecipe) {
 
   // UpdateRecipeData
 };
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmars);
   recipieView.addHandlerRender(controlRecipes);
@@ -140,7 +138,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
